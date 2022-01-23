f903ad94ac9d13f8d085a5587302123f1de7ba79
Aniket Agarwal <aniketagarwal57@gmail.com>
2022-01-23 18:56:53 +0530

    Update Navbar.js

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -10,29 +10,32 @@ export default function Navbar() {
           <div className="flex items-center justify-between h-16">
             <div className="flex items-center">
               <div className="flex-shrink-0 text-white text-lg font-large">
-                Colosseum
+                <a href="/"> Colosseum</a>
               </div>
               <div className="hidden md:block">
                 <div className="ml-10 flex items-baseline space-x-4">
                   <a
-                    href="#"
+                    href="/"
                     className=" hover:bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium"
                   >
                     Home
                   </a>
 
                   <a
-                    href="#"
+                    href="/recommendation"
                     className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                   >
                     Recommendation
                   </a>
                   <a
-                    href="#"
+                    href="/schedule"
                     className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                   >
                     Schedule
                   </a>
+                </div>
+                <div className="flex items-baseline space-x-4">
+                  <button>Add Event</button>
                 </div>
               </div>
             </div>
