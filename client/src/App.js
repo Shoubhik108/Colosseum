a4afaee105cd161bf3cf8504f5f1f9a2e77c96be
Aniket Agarwal <aniketagarwal57@gmail.com>
2022-01-23 18:48:27 +0530

    Add navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
 import React, { useContext } from "react";
-import Main from "./Main"
-import Login from "./Pages/Login/Login"
+import Navbar from "./components/Navbar/Navbar";
+import Main from "./Main";
+import Login from "./Pages/Login/Login";
 
 function App() {
-
   // const user = useContext(AuthContext);
 
   return (
     <>
       <div className="App">
+        <Navbar />
         <Main />
         {/* {user ? (
           <Main />
